ce3f323d52c6b4186a7f0e4b97de6bb6be6b78e8
Damon Dudek <damondudek@outlook.com>
2020-07-06 21:23:43 -0300

    Criado o login e sql do usuário

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
               <a class="nav-link" href="#">Contato</a>
             </li>
           </ul>
+          <button onclick="entrar()" class="btn btn-outline-success my-2 my-sm-0" type="submit">Entrar</button>
         </div>
       </nav>
     </div>
@@ -112,5 +113,6 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="./js/main.js"></script>
   </body>
 </html>
